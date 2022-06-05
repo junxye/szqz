7dd1667774d06d17e20f14533b11691686bf17b2
junxye <1390290357@qq.com>
2022-06-05 10:59:51 +0800

    add

--- src/main/java/com/szqz/config/SecurityConfig.java
+++ src/main/java/com/szqz/config/SecurityConfig.java
@@ -12,39 +12,8 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-/*
-public class SecurityConfig extends WebSecurityConfiguration {
-
-        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-        String password = passwordEncoder.encode("123");
-        auth.inMemoryAuthentication().withUser("lucy").password(password).roles("admin");
 
 
-    protected void configure(HttpSecurity http) throws Exception{
-        //配置url的访问权限
-        http.authorizeRequests()
-                .antMatchers("/").permitAll()
-                .antMatchers("/**update**").permitAll()
-                .antMatchers("/login/**").permitAll()
-                .anyRequest().authenticated();
-
-        //关闭csrf保护功能
-        //http.csrf().disable();
-//
-        //使用自定义的登录窗口
-        http.formLogin()
-                .loginPage("/userLogin").permitAll()
-                .usernameParameter("username").passwordParameter("password")
-                .defaultSuccessUrl("/")
-                .failureUrl("/userLogin?error");
-    }
-
-    @Bean
-    PasswordEncoder password(){
-        return new BCryptPasswordEncoder();
-    }
-}
-*/
 /**
  * SpringSecurity 核心配置类
  * prePostEnabled = true 开启注解权限认证功能
@@ -104,39 +73,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
      */
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-/*
-        http
-                // 关闭csrf(前后端分离项目要关闭此功能）
-                .csrf().disable()
-                // 禁用session (前后端分离项目，不通过Session获取SecurityContext)
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
-                // 请求认证配置
-                .authorizeRequests()
-                // 允许匿名访问：未登录可以访问，已登录不能访问
-                .antMatchers("/login").anonymous()
-                // .antMatchers("/login").permitAll()// 登录或未登录都能访问
-                // .antMatchers("/textMybatis").hasAuthority("system:dept:list22")
-                // 任意用户，认证之后才可以访问（除上面外的）
-                .anyRequest().authenticated();
 
-        // 添加token过滤器
-        http.addFilterBefore(jwtAuthenticationTokenFilter, UsernamePasswordAuthenticationFilter.class);
-
-        // 配置异常处理器
-        http.exceptionHandling()
-                // 认证失败
-                .authenticationEntryPoint(authenticationEntryPoint)
-                // 授权失败
-                .accessDeniedHandler(accessDeniedHandler);
-
-        // spring security 允许跨域
-        http.cors();
-*/
         //cors()解决跨域问题，csrf()会与restful风格冲突，默认springsecurity是开启的，所以要disable()关闭一下
         http.cors().and().csrf().disable();
 
-        //     /index需要权限为ROLE_USER才能访问   /hello需要权限为ROLE_ADMIN才能访问
         http.authorizeRequests()
                 .antMatchers("/user").hasAnyRole("USER", "ADMIN")
                 .antMatchers("/index").hasRole("USER")
